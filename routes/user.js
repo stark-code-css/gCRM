d3ffd0f212c5ad813e2fa43202934f54b0ab4174
Stark <stark.code.css@gmail.com>
2024-05-16 16:51:33 +0530

    removed register

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { register, login } = require('../controller/user.controller')
 
-router.post('/register', register)
+// router.post('/register', register)
 router.post('/login', login)
 
 module.exports = router
